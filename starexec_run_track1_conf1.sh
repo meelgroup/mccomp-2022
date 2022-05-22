9bcd967cc4aee1feebec84d651bafcc43bf79287
Mate Soos <soos.mate@gmail.com>
2022-05-22 21:03:35 +0200

    Improving

--- starexec_run_track1_conf1.sh
+++ starexec_run_track1_conf1.sh
@@ -13,7 +13,6 @@ echo "c o solfile: $solfile  indfile: $indfile  cleanfile: $cleanfile cleancnffi
 
 timeout_value=$(( STAREXEC_WALLCLOCK_LIMIT ))
 tout_be=110
-SECONDS=0
 
 echo "c o This script is for regular model counting track"
 grep -v "^c" $file > $cleanfile
@@ -23,9 +22,11 @@ found=`grep "^p cnf" $preprocessed_cnf_file`
 if [[ $found == *"p cnf"* ]]; then
    echo "c o OK, Arjun succeeded"
    grep -v "^c" $preprocessed_cnf_file > $cleancnffile
+   multi=`grep "^c MUST MUTIPLY BY" $preprocessed_cnf_file| awk '{print $5}'`
 else
    echo "c o WARNING Arjun did NOT succeed"
    grep -v "^c" $file > $cleancnffile
+   multi=1
 fi
 cache_size=$(( STAREXEC_MAX_MEM/2 ))
 echo "c o Trying to run Ganak, cache_size: ${cache_size}"
@@ -35,6 +36,7 @@ if [[ $solved_by_ganak == *"SATISFIABLE"* ]]; then
     sed -E "s/^(.)/c o \1/" $solfile
     sat=`grep "^s .*SATISFIABLE" $solfile`
     count=`grep "^s .*mc" $solfile | awk '{print $3}'`
+    count=`echo "$count*$multi" | bc -l`
     log_10_count=`echo "scale=15; l($count)/l(10)" | bc -l `
 
     echo $sat
