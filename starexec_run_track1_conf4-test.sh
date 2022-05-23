3e53d5ffbe32aafea66436bf19761886663e2f2a
Mate Soos <soos.mate@gmail.com>
2022-05-23 21:37:13 +0200

    Adding test setups

---
+++ starexec_run_track1_conf4-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+file=$1
+mc=`grep "^c t " $file`
+echo "c o found header: $mc"
+
+cache_size=$(( STAREXEC_MAX_MEM/2 ))
+echo "c o This script is for regular model counting"
+./sharpSAT-td -decot 10 -decow 100 -tmpdir . -cs ${cache_size} $1
+
